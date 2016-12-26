ecef174a5a63b7948e50953a377e7cd16e08c0db
BabyManisha <babymaneesha@gmail.com>
2016-12-26 20:46:14 +0530

    new js1

--- home.js
+++ home.js
@@ -4,7 +4,6 @@ getFriendsList = function() {
                         console.log(response);
             });
 };
-alert("Hello");
 
 // The SDK loaded callback (see below)
 window.fbAsyncInit = function() {
